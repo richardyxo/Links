6e959899ce6da826ada1638d20efd875e2e987dd
richardyxo <diasmarlene982@gmail.com>
2023-02-10 15:01:43 -0300

    add more style for the page

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-function typewriterEffect(element, delay = 0){
-  let split_element=element.innerHTML.split('');
-  element.innerHTML='';
+function typewriterEffect(element, delay = 0) {
+  let split_element = element.innerHTML.split("")
+  element.innerHTML = ""
   setTimeout(() => {
-      split_element.forEach((value, index) => {
-        setTimeout(() => {
-          element.innerHTML += value
-        }, 60 * index)
-      })
-  },delay)
+    split_element.forEach((value, index) => {
+      setTimeout(() => {
+        element.innerHTML += value
+      }, 35 * index)
+    })
+  }, delay)
 }
-typewriterEffect(document.getElementById('texto'))
+typewriterEffect(document.getElementById("texto"))
